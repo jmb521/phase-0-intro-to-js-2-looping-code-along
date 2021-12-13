96c155c4ceb021fbf9386941db11ed70af7731d7
Jennifer Pazos <jmp521@gmail.com>
2021-12-13 17:47:14 -0600

    commit tree 24ccb5b010ab4ca7e1df1885326bb6f9138088b4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let newArr = []
+    for(let i = 0; i< array.length; i++) {
+        newArr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+        // Thank you, Charlie, for the wonderful birthday gift!",
+    }
+    return newArr
+}
+
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number)
+        number --
+    }
+}
